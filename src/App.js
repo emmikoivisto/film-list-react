eb4ea1461e82371ae0678f9d8ce33df0dcb229ba
emmikoivisto <emetsi@gmail.com>
2020-12-07 15:57:02 +0000

    film console

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import FilmConsole from './containers/FilmConsole';
 
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <FilmConsole></FilmConsole>
+    </>
   );
 }
 
